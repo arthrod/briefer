4cbaed5f707edb64419a124a2e20654560ef18ed
arthrod <arthursrodrigues@gmail.com>
2025-02-15 22:56:26 -0500

    db issues

--- docker/setup/setup.py
+++ docker/setup/setup.py
@@ -1,25 +1,29 @@
 import grp
+import json
+import logging
+import os
 import pwd
+import subprocess
 import time
 from pathlib import Path
-import os
-import psycopg2
-import json
-import subprocess
-import logging
 
-APPS_CONFIG_DIR = Path("/home/briefer", ".config", "briefer")
-JUPYTER_CONFIG_DIR = Path("/home/jupyteruser", ".config", "briefer")
+import psycopg2
+
+APPS_CONFIG_DIR = Path('/home/briefer', '.config', 'briefer')
+JUPYTER_CONFIG_DIR = Path('/home/jupyteruser', '.config', 'briefer')
+
 
 def get_random_secret(size=32):
     return os.urandom(size).hex()
 
+
 def get_config_path(dir):
-    return Path(dir, "briefer.json")
+    return Path(dir, 'briefer.json')
+
 
 def get_config(dir):
     fpath = get_config_path(dir)
-    with open(fpath, "r") as f:
+    with open(fpath, 'r') as f:
         cfg = json.load(f)
 
         # override config with env vars
@@ -31,24 +35,26 @@ def get_config(dir):
 
         return cfg
 
+
 def generate_default_config():
     return {
-        "NODE_ENV": "production",
-        "LOG_LEVEL": "info",
-        "POSTGRES_USERNAME": "briefer",
-        "POSTGRES_PASSWORD": get_random_secret(8),
-        "POSTGRES_HOSTNAME": "localhost",
-        "POSTGRES_PORT": "5432",
-        "POSTGRES_DATABASE": "briefer",
-        "JUPYTER_TOKEN": get_random_secret(32),
-        "AI_BASIC_AUTH_USERNAME": get_random_secret(8),
-        "AI_BASIC_AUTH_PASSWORD": get_random_secret(8),
-        "LOGIN_JWT_SECRET": get_random_secret(),
-        "AUTH_JWT_SECRET": get_random_secret(),
-        "ENVIRONMENT_VARIABLES_ENCRYPTION_KEY": get_random_secret(32),
-        "DATASOURCES_ENCRYPTION_KEY": get_random_secret(32),
-        "WORKSPACE_SECRETS_ENCRYPTION_KEY": get_random_secret(32),
+        'NODE_ENV': 'production',
+        'LOG_LEVEL': 'info',
+        'POSTGRES_USERNAME': 'briefer',
+        'POSTGRES_PASSWORD': 'briefer',
+        'POSTGRES_HOSTNAME': 'localhost',
+        'POSTGRES_PORT': '5432',
+        'POSTGRES_DATABASE': 'briefer',
+        'JUPYTER_TOKEN': get_random_secret(32),
+        'AI_BASIC_AUTH_USERNAME': get_random_secret(8),
+        'AI_BASIC_AUTH_PASSWORD': get_random_secret(8),
+        'LOGIN_JWT_SECRET': get_random_secret(),
+        'AUTH_JWT_SECRET': get_random_secret(),
+        'ENVIRONMENT_VARIABLES_ENCRYPTION_KEY': get_random_secret(32),
+        'DATASOURCES_ENCRYPTION_KEY': get_random_secret(32),
+        'WORKSPACE_SECRETS_ENCRYPTION_KEY': get_random_secret(32),
     }
+
 
 def generate_apps_config():
     fpath = get_config_path(APPS_CONFIG_DIR)
@@ -59,9 +65,9 @@ def generate_apps_config():
         if k in os.environ:
             cfg[k] = os.environ[k]
 
-    with open(fpath, "w") as f:
+    with open(fpath, 'w') as f:
         json.dump(cfg, f, indent=4)
-    os.chown(fpath, pwd.getpwnam("briefer").pw_uid, grp.getgrnam("briefer").gr_gid)
+    os.chown(fpath, pwd.getpwnam('briefer').pw_uid, grp.getgrnam('briefer').gr_gid)
     os.chmod(fpath, 0o700)
 
     return cfg
@@ -71,61 +77,61 @@ def setup_apps():
     config_path = get_config_path(APPS_CONFIG_DIR)
     is_first_run = not config_path.exists()
     if is_first_run:
-        logging.info("First run, generating apps config")
+        logging.info('First run, generating apps config')
         cfg = generate_apps_config()
     else:
-        logging.info("Apps config exists, loading")
+        logging.info('Apps config exists, loading')
         cfg = get_config(APPS_CONFIG_DIR)
 
-    logging.info("Setting up postgres")
+    logging.info('Setting up postgres')
     while True:
         try:
-            with psycopg2.connect(user="briefer", password="briefer", host="localhost", port="5432") as conn:
+            with psycopg2.connect(user='briefer', password='briefer', host='localhost', port='5432') as conn:
                 break
         except:
-            logging.info("Waiting for postgres to be ready")
+            logging.info('Waiting for postgres to be ready')
             time.sleep(0.3)
             continue
-    with psycopg2.connect(user="briefer", password="briefer", host="localhost", port="5432") as conn:
-        logging.info("Postgres is ready")
-        logging.info("Changing default user password")
+    with psycopg2.connect(user='briefer', password='briefer', host='localhost', port='5432') as conn:
+        logging.info('Postgres is ready')
+        logging.info('Changing default user password')
         cur = conn.cursor()
         cur.execute(f"ALTER USER briefer WITH PASSWORD '{cfg['POSTGRES_PASSWORD']}'")
         conn.commit()
-        logging.info("Password changed")
-
+        logging.info('Password changed')
 
     run_migrations(cfg)
 
     return cfg
 
+
 def generate_jupyter_config():
     fpath = get_config_path(JUPYTER_CONFIG_DIR)
     apps_cfg = get_config(APPS_CONFIG_DIR)
-    cfg = {
-        "JUPYTER_TOKEN": apps_cfg["JUPYTER_TOKEN"],
-    }
-    with open(fpath, "w") as f:
+    cfg = {'JUPYTER_TOKEN': apps_cfg['JUPYTER_TOKEN']}
+    with open(fpath, 'w') as f:
         json.dump(cfg, f, indent=4)
 
     # recursively chown jupyteruser home directory
-    os.system(f"chown -R jupyteruser:jupyteruser /home/jupyteruser")
-    os.system(f"chmod -R 700 /home/jupyteruser")
+    os.system('chown -R jupyteruser:jupyteruser /home/jupyteruser')
+    os.system('chmod -R 700 /home/jupyteruser')
+
 
 def setup_jupyter():
     generate_jupyter_config()
 
-def run_migrations(cfg):
-    logging.info("Running migrations")
 
-    username = cfg["POSTGRES_USERNAME"]
-    password = cfg["POSTGRES_PASSWORD"]
-    hostname = cfg["POSTGRES_HOSTNAME"]
-    port = cfg["POSTGRES_PORT"]
-    database = cfg["POSTGRES_DATABASE"]
+def run_migrations(cfg):
+    logging.info('Running migrations')
+
+    username = cfg['POSTGRES_USERNAME']
+    password = cfg['POSTGRES_PASSWORD']
+    hostname = cfg['POSTGRES_HOSTNAME']
+    port = cfg['POSTGRES_PORT']
+    database = cfg['POSTGRES_DATABASE']
     default_env = {
-        "NODE_ENV": "production",
-        "POSTGRES_PRISMA_URL": f"postgresql://{username}:{password}@{hostname}:{port}/{database}?schema=public"
+        'NODE_ENV': 'production',
+        'POSTGRES_PRISMA_URL': f'postgresql://{username}:{password}@{hostname}:{port}/{database}?schema=public',
     }
 
     env = os.environ.copy()
@@ -134,24 +140,24 @@ def run_migrations(cfg):
             env[k] = v
 
     migrations = subprocess.run(
-        ["npx", "prisma", "migrate", "deploy", "--schema", "packages/database/prisma/schema.prisma"],
+        ['npx', 'prisma', 'migrate', 'deploy', '--schema', 'packages/database/prisma/schema.prisma'],
         env=env,
-        cwd="/app/api/"
+        cwd='/app/api/',
     )
     migrations.check_returncode()
 
-    logging.info("Migrations done")
+    logging.info('Migrations done')
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
-    logging.info("Starting setup")
+    logging.info('Starting setup')
 
     # create an empty file to signal that setup is running
-    setups = [(APPS_CONFIG_DIR, "briefer"), (JUPYTER_CONFIG_DIR, "jupyteruser")]
+    setups = [(APPS_CONFIG_DIR, 'briefer'), (JUPYTER_CONFIG_DIR, 'jupyteruser')]
     for dir, user in setups:
         os.makedirs(dir, exist_ok=True)
-        path = Path(dir, "setup")
+        path = Path(dir, 'setup')
         path.touch()
         os.chown(path, pwd.getpwnam(user).pw_uid, grp.getgrnam(user).gr_gid)
 
@@ -159,10 +165,10 @@ def main():
     setup_jupyter()
 
     for path, user in setups:
-        Path(path, "setup").unlink()
+        Path(path, 'setup').unlink()
 
-    logging.info("Setup finished")
+    logging.info('Setup finished')
+
 
 if __name__ == '__main__':
     main()
-
